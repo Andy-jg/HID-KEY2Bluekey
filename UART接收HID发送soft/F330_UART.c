f0abcf29fe46ac68bdf797d76c10e3ec5e37c56e
jiajia <674663334@qq.com>
2018-10-08 22:19:05 +0800

    格式上更改。排版从新做。

--- UART接收HID发送soft/F330_UART.c
+++ UART接收HID发送soft/F330_UART.c
@@ -14,7 +14,7 @@ static uint8 poweroff_flag=0;//关机标志
 static uint8 bat_val=0;//电池电压
 static uint8 adc_flag=0;//电池电压测试标志，10min
 static uint16 adc_timeout=0;//电压测试时间
-uint8 code batt[16]={0x27,0x1E,0x1F,0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x05,0x04,0x17,0x10,0x0C,0x11};//0~9,bat,min
+uint8 code batt[16]= {0x27,0x1E,0x1F,0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x05,0x04,0x17,0x10,0x0C,0x11}; //0~9,bat,min
 
 #define vcc_on	1
 #define vcc_off	0
@@ -53,7 +53,7 @@ void delay_ms(uint16 ms);
 //-----------------------------------------------------------------------------
 // Global Variables
 //-----------------------------------------------------------------------------
-static uint8 tx_buf[12]={0x0C,0x00,0xA1,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};//HID发送缓冲区
+static uint8 tx_buf[12]= {0x0C,0x00,0xA1,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}; //HID发送缓冲区
 static uint8 tx_count;//发送字节数，实际发送12个字节
 uint8 fn_flag;//Fn键按下状态
 #define FnDown	0x00
@@ -88,42 +88,46 @@ uint8 TX_Ready =1;
 static uint8 Byte;
 
 //KEY
-uint8 code key_code[128]={
+uint8 code key_code[128]=
+{
 
-0x00,0x00,0x65,0x00,0x00,0x00,0x00,0xE3,0x29,0x00,0x00,0x00,0x00,0x2B,0x35,0x00,
+	0x00,0x00,0x65,0x00,0x00,0x00,0x00,0xE3,0x29,0x00,0x00,0x00,0x00,0x2B,0x35,0x00,
 
-0xE2,0x00,0xE1,0xE6,0xE0,0x14,0x1E,0x00,0x00,0x00,0x1D,0x16,0x04,0x1A,0x1F,0x00,
+	0xE2,0x00,0xE1,0xE6,0xE0,0x14,0x1E,0x00,0x00,0x00,0x1D,0x16,0x04,0x1A,0x1F,0x00,
 
-0x00,0x06,0x1B,0x07,0x08,0x21,0x20,0x00,0x52,0x00,0x19,0x09,0x17,0x15,0x22,0x4F,
+	0x00,0x06,0x1B,0x07,0x08,0x21,0x20,0x00,0x52,0x00,0x19,0x09,0x17,0x15,0x22,0x4F,
 
-0x00,0x11,0x05,0x0B,0x0A,0x1C,0x23,0x00,0x00,0x00,0x10,0x0D,0x18,0x24,0x25,0x00,
+	0x00,0x11,0x05,0x0B,0x0A,0x1C,0x23,0x00,0x00,0x00,0x10,0x0D,0x18,0x24,0x25,0x00,
 
-0x00,0x36,0x0E,0x0C,0x12,0x27,0x26,0x00,0x00,0x37,0x38,0x0F,0x33,0x13,0x2D,0x00,
+	0x00,0x36,0x0E,0x0C,0x12,0x27,0x26,0x00,0x00,0x37,0x38,0x0F,0x33,0x13,0x2D,0x00,
 
-0x00,0x00,0x34,0x00,0x2F,0x2E,0x00,0x00,0x39,0xE5,0x28,0x30,0x2C,0x31,0x50,0x00,
+	0x00,0x00,0x34,0x00,0x2F,0x2E,0x00,0x00,0x39,0xE5,0x28,0x30,0x2C,0x31,0x50,0x00,
 
-0x51,0x00,0x00,0x00,0x00,0x00,0x4C,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x51,0x00,0x00,0x00,0x00,0x00,0x4C,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
 
-0x00,0x2A,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,};
+	0x00,0x2A,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+};
 
 //Fn_KEY
-uint8 code fn_key_code[128]={
+uint8 code fn_key_code[128]=
+{
 
-0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x29,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x29,0x00,
 
-0x00,0x00,0x00,0x00,0x00,0x00,0x3A,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x3B,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x3A,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x3B,0x00,
 
-0x00,0x00,0x00,0x00,0x00,0x3D,0x3C,0x00,0x4B,0x00,0x00,0x00,0x00,0x00,0x3E,0x4D,
+	0x00,0x00,0x00,0x00,0x00,0x3D,0x3C,0x00,0x4B,0x00,0x00,0x00,0x00,0x00,0x3E,0x4D,
 
-0x00,0x00,0x00,0x00,0x00,0x00,0x3F,0x00,0x00,0x00,0x00,0x00,0x00,0x40,0x41,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x3F,0x00,0x00,0x00,0x00,0x00,0x00,0x40,0x41,0x00,
 
-0x00,0x00,0x00,0x00,0x00,0x43,0x42,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x44,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x43,0x42,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x44,0x00,
 
-0x00,0x00,0x00,0x00,0x00,0x45,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x4A,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x45,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x4A,0x00,
 
-0x4E,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+	0x4E,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
 
-0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,};
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+};
 
 //-----------------------------------------------------------------------------
 // MAIN Routine
@@ -133,7 +137,7 @@ void main (void)
 {
 	uint8 i,tmp;
 	PCA0MD &= ~0x40;                    // WDTE = 0 (clear watchdog timer
-	                                   // enable)
+	// enable)
 	PORT_Init();                        // Initialize Port I/O
 	SYSCLK_Init ();                     // Initialize Oscillator
 	UART0_Init();
@@ -151,7 +155,7 @@ void main (void)
 	adc_flag=0;
 
 
-   	//'0c 00 a1 01 00 00 27 00 00 00 00 00
+	//'0c 00 a1 01 00 00 27 00 00 00 00 00
 	tx_buf[0]=0x0C;//固定开头
 	tx_buf[1]=0x00;
 	tx_buf[2]=0xA1;
@@ -170,20 +174,23 @@ void main (void)
 	fn_flag=FnUp;
 
 	ADC0_Init();
-	bl_vcc_ctrl	=	bl_on;/// /1为关闭蓝牙电源，0为开启
+	bl_vcc_ctrl	=	bl_on;//1为关闭蓝牙电源，0为开启
 	EA = 1;
-	
-	
+
+
 	while(1)
 	{
 		delay_ms(10);
-		
+
 		if(TX_Ready == 1 && UART_Buffer_Size != 0)
 		{
 			TX_Ready = 0;                 // Set the flag to zero
 
 			// If a new word is being output
-			if ( UART_Buffer_Size == UART_Input_First ) {UART_Output_First = 0;}
+			if ( UART_Buffer_Size == UART_Input_First )
+			{
+				UART_Output_First = 0;
+			}
 
 			tmp = UART_Buffer[UART_Output_First];
 
@@ -206,11 +213,11 @@ void main (void)
 		{
 			idle_timeout=0;//重新连接按下则空闲等待置0，从新计数
 			reconn_flag=0;
-			
+
 			bl_conn_ctrl=1;//蓝牙清楚记忆，重新连接
 			delay_ms(2000);
 			bl_conn_ctrl=0;
-			
+
 		}
 
 		if(poweroff_flag)
@@ -218,7 +225,7 @@ void main (void)
 			poweroff_flag=0;
 			vcc_ctrl=vcc_off;//总电源关闭
 		}
-		
+
 		if(adc_flag)
 		{
 			adc_flag=0;
@@ -257,17 +264,17 @@ void ADC0_Init()
 {
 	uint8 k;
 	uint16 adc_val;
-	
+
 	EA=0;
 	delay_ms(50);
 
-	
+
 	AMX0P     = 0x0F;//P1.7
-    AMX0N     = 0x11;//GND
+	AMX0N     = 0x11;//GND
 	ADC0CF   |= 0x04;//LEFT ADJUST
 	AD0EN	  = 1;
 	adc_val	  = 0;
-	for(k=0;k<10;k++)
+	for(k=0; k<10; k++)
 	{
 		AD0INT=0;
 		AD0BUSY=1;//转换ADC
@@ -277,7 +284,7 @@ void ADC0_Init()
 	AD0EN = 0;
 
 	k=(uint8)(adc_val / 5)+3;//补偿3-36mv，4-48mv
-	
+
 	if(k>115)bat_val=100;//2776
 	else if(k>110)bat_val=75;//2655
 	else if(k>105)bat_val=50;//2535
@@ -308,9 +315,9 @@ void ADC0_Init()
 void SYSCLK_Init (void)
 {
 
-   OSCICN    = 0x81;					//6.125MHz
+	OSCICN    = 0x81;					//6.125MHz
 
-   RSTSRC  = 0x04;                     // Enable missing clock detector
+	RSTSRC  = 0x04;                     // Enable missing clock detector
 
 }
 //1ms定时器
@@ -322,7 +329,7 @@ void Timer0_Init (void)
 	TL0 = TH0;                          // init Timer1
 	TMOD &= ~0x0F;                      // TMOD: timer 1 in 8-bit autoreload
 	TMOD |=  0x02;
-	TR0 = 1;  
+	TR0 = 1;
 	ET0=1;
 }
 //-----------------------------------------------------------------------------
@@ -336,35 +343,42 @@ void Timer0_Init (void)
 //-----------------------------------------------------------------------------
 void UART0_Init (void)
 {
-   SCON0 = 0x10;                       // SCON0: 8-bit variable bit rate
-                                       //        level of STOP bit is ignored
-                                       //        RX enabled
-                                       //        ninth bits are zeros
-                                       //        clear RI0 and TI0 bits
-   if (SYSTEMCLOCK/BAUDRATE/2/256 < 1) {
-      TH1 = -(SYSTEMCLOCK/BAUDRATE/2);
-      CKCON &= ~0x0B;                  // T1M = 1; SCA1:0 = xx
-      CKCON |=  0x08;
-   } else if (SYSTEMCLOCK/BAUDRATE/2/256 < 4) {
-      TH1 = -(SYSTEMCLOCK/BAUDRATE/2/4);
-      CKCON &= ~0x0B;                  // T1M = 0; SCA1:0 = 01
-      CKCON |=  0x01;
-   } else if (SYSTEMCLOCK/BAUDRATE/2/256 < 12) {
-      TH1 = -(SYSTEMCLOCK/BAUDRATE/2/12);
-      CKCON &= ~0x0B;                  // T1M = 0; SCA1:0 = 00
-   } else {
-      TH1 = -(SYSTEMCLOCK/BAUDRATE/2/48);
-      CKCON &= ~0x0B;                  // T1M = 0; SCA1:0 = 10
-      CKCON |=  0x02;
-   }
+	SCON0 = 0x10;                       // SCON0: 8-bit variable bit rate
+	//        level of STOP bit is ignored
+	//        RX enabled
+	//        ninth bits are zeros
+	//        clear RI0 and TI0 bits
+	if (SYSTEMCLOCK/BAUDRATE/2/256 < 1)
+	{
+		TH1 = -(SYSTEMCLOCK/BAUDRATE/2);
+		CKCON &= ~0x0B;                  // T1M = 1; SCA1:0 = xx
+		CKCON |=  0x08;
+	}
+	else if (SYSTEMCLOCK/BAUDRATE/2/256 < 4)
+	{
+		TH1 = -(SYSTEMCLOCK/BAUDRATE/2/4);
+		CKCON &= ~0x0B;                  // T1M = 0; SCA1:0 = 01
+		CKCON |=  0x01;
+	}
+	else if (SYSTEMCLOCK/BAUDRATE/2/256 < 12)
+	{
+		TH1 = -(SYSTEMCLOCK/BAUDRATE/2/12);
+		CKCON &= ~0x0B;                  // T1M = 0; SCA1:0 = 00
+	}
+	else
+	{
+		TH1 = -(SYSTEMCLOCK/BAUDRATE/2/48);
+		CKCON &= ~0x0B;                  // T1M = 0; SCA1:0 = 10
+		CKCON |=  0x02;
+	}
 
-   TL1 = TH1;                          // init Timer1
-   TMOD &= ~0xf0;                      // TMOD: timer 1 in 8-bit autoreload
-   TMOD |=  0x20;
-   TR1 = 1;                            // START Timer1
-   TX_Ready = 1;                       // Flag showing that UART can transmit
-   IP |= 0x10;                         // Make UART high priority
-   ES0 = 1;                            // Enable UART0 interrupts
+	TL1 = TH1;                          // init Timer1
+	TMOD &= ~0xf0;                      // TMOD: timer 1 in 8-bit autoreload
+	TMOD |=  0x20;
+	TR1 = 1;                            // START Timer1
+	TX_Ready = 1;                       // Flag showing that UART can transmit
+	IP |= 0x10;                         // Make UART high priority
+	ES0 = 1;                            // Enable UART0 interrupts
 }
 
 //-----------------------------------------------------------------------------
@@ -375,25 +389,25 @@ void UART0_Init (void)
 //-----------------------------------------------------------------------------
 void Timer0_ISR (void) interrupt 1
 {
-   static int counter = 0;
+	static int counter = 0;
 
-   if((counter++) >= 1000)
-   {
-      counter = 0;
-	  idle_timeout++;
-	  adc_timeout++;
-		
-	  if(idle_timeout>=idle_timeout_time)//5min
-	  {
-		idle_timeout=0;
-		poweroff_flag=1;
-	  }
-	  if(adc_timeout>=600)//10min
-	  {
-	  	adc_timeout=0;
-		adc_flag=1;
-	  }
-   }
+	if((counter++) >= 1000)
+	{
+		counter = 0;
+		idle_timeout++;
+		adc_timeout++;
+
+		if(idle_timeout>=idle_timeout_time)//5min
+		{
+			idle_timeout=0;
+			poweroff_flag=1;
+		}
+		if(adc_timeout>=600)//10min
+		{
+			adc_timeout=0;
+			adc_flag=1;
+		}
+	}
 }
 //-----------------------------------------------------------------------------
 // UART0_Interrupt
@@ -406,44 +420,46 @@ void Timer0_ISR (void) interrupt 1
 
 void UART0_Interrupt (void) interrupt 4
 {
-   idle_timeout=0;//有按键接收则，空闲等待时间置0，从新计数
-   
-   if (RI0 == 1)
-   {
-      if( UART_Buffer_Size == 0)  {      // If new word is entered
-         UART_Input_First = 0;    }
+	idle_timeout=0;//有按键接收则，空闲等待时间置0，从新计数
 
-      RI0 = 0;                           // Clear interrupt flag
+	if (RI0 == 1)
+	{
+		if( UART_Buffer_Size == 0)         // If new word is entered
+		{
+			UART_Input_First = 0;
+		}
 
-      Byte = SBUF0;                      // Read a character from UART
+		RI0 = 0;                           // Clear interrupt flag
 
-      if (UART_Buffer_Size < UART_BUFFERSIZE)
-      {
-         UART_Buffer[UART_Input_First] = Byte; // Store in array
+		Byte = SBUF0;                      // Read a character from UART
 
-         UART_Buffer_Size++;             // Update array's size
+		if (UART_Buffer_Size < UART_BUFFERSIZE)
+		{
+			UART_Buffer[UART_Input_First] = Byte; // Store in array
 
-         UART_Input_First++;             // Update counter
-      }
-   }
+			UART_Buffer_Size++;             // Update array's size
 
-   if (TI0 == 1)                   // Check if transmit flag is set
-   {
-      TI0 = 0;                           // Clear interrupt flag
+			UART_Input_First++;             // Update counter
+		}
+	}
 
-      if (tx_count <12)         // 总共发送12个字节
-      {
+	if (TI0 == 1)                   // Check if transmit flag is set
+	{
+		TI0 = 0;                           // Clear interrupt flag
 
-         // Store a character in the variable byte
-         SBUF0= tx_buf[tx_count++];
+		if (tx_count <12)         // 总共发送12个字节
+		{
 
-      }
-      else
-      {
-         tx_count = 0;            // Set the array size to 0
-         TX_Ready = 1;                    // Indicate transmission complete
-      }
-   }
+			// Store a character in the variable byte
+			SBUF0= tx_buf[tx_count++];
+
+		}
+		else
+		{
+			tx_count = 0;            // Set the array size to 0
+			TX_Ready = 1;                    // Indicate transmission complete
+		}
+	}
 }
 
 //键值处理
@@ -451,155 +467,155 @@ void KEY_VAL(uint8 tmp)
 {
 	switch (tmp)
 	{
-		case LCtrl:	 
+	case LCtrl:
+	{
+		tx_buf[4]|=0x01;
+		break;
+	}
+	case LShift:
+	{
+		tx_buf[4]|=0x02;
+		break;
+	}
+	case LAlt:
+	{
+		tx_buf[4]|=0x04;
+		break;
+	}
+	case LGui:
+	{
+		tx_buf[4]|=0x08;
+		break;
+	}
+	case RShift:
+	{
+		tx_buf[4]|=0x20;
+		break;
+	}
+	case RAlt:
+	{
+		tx_buf[4]|=0x40;
+		break;
+	}
+	case LCtrl+0x80:
+	{
+		tx_buf[4]&=~0x01;
+		break;
+	}
+	case LShift+0x80:
+	{
+		tx_buf[4]&=~0x02;
+		break;
+	}
+	case LAlt+0x80:
+	{
+		tx_buf[4]&=~0x04;
+		break;
+	}
+	case LGui+0x80:
+	{
+		tx_buf[4]&=~0x08;
+		break;
+	}
+	case RShift+0x80:
+	{
+		tx_buf[4]&=~0x20;
+		break;
+	}
+	case RAlt+0x80:
+	{
+		tx_buf[4]&=~0x40;
+		break;
+	}
+	case Fn:
+	{
+		fn_flag=FnDown;
+		tx_buf[6]=0x00;
+		break;
+	}
+	case Fn+0x80:
+	{
+		fn_flag=FnUp;
+		tx_buf[6]=0x00;
+		tx_buf[7]=0x00;
+		tx_buf[8]=0x00;
+		tx_buf[9]=0x00;
+		tx_buf[10]=0x00;
+		tx_buf[11]=0x00;
+		break;
+	}
+	default :
+	{
+		if(tmp & 0x80)
 		{
-			tx_buf[4]|=0x01;
-			break;
-		}
-		case LShift:	 
-		{
-			tx_buf[4]|=0x02;
-			break;
-		}
-		case LAlt:	 
-		{
-			tx_buf[4]|=0x04;
-			break;
-		}
-		case LGui:	 
-		{
-			tx_buf[4]|=0x08;
-			break;
-		}
-		case RShift:	 
-		{
-			tx_buf[4]|=0x20;
-			break;
-		}
-		case RAlt:	 
-		{
-			tx_buf[4]|=0x40;
-			break;
-		}
-		case LCtrl+0x80:	 
-		{
-			tx_buf[4]&=~0x01;
-			break;
-		}
-		case LShift+0x80:	 
-		{
-			tx_buf[4]&=~0x02;
-			break;
-		}
-		case LAlt+0x80:	 
-		{
-			tx_buf[4]&=~0x04;
-			break;
-		}
-		case LGui+0x80:	 
-		{
-			tx_buf[4]&=~0x08;
-			break;
-		}
-		case RShift+0x80:	 
-		{
-			tx_buf[4]&=~0x20;
-			break;
-		}
-		case RAlt+0x80:	 
-		{
-			tx_buf[4]&=~0x40;
-			break;
-		}		
-		case Fn:	 
-		{
-			fn_flag=FnDown;
-			tx_buf[6]=0x00;
-			break;
-		}
-		case Fn+0x80:	 
-		{
-			fn_flag=FnUp;
 			tx_buf[6]=0x00;
 			tx_buf[7]=0x00;
 			tx_buf[8]=0x00;
 			tx_buf[9]=0x00;
 			tx_buf[10]=0x00;
 			tx_buf[11]=0x00;
-			break;
 		}
-		default :
+		else
 		{
-			if(tmp & 0x80)
+			if(fn_flag==FnUp)
 			{
-				tx_buf[6]=0x00;
-				tx_buf[7]=0x00;
-				tx_buf[8]=0x00;
-				tx_buf[9]=0x00;
-				tx_buf[10]=0x00;
-				tx_buf[11]=0x00;
+				tx_buf[6]=key_code[tmp];
 			}
-			else
-			{
-				if(fn_flag==FnUp)	
-				{
-					tx_buf[6]=key_code[tmp];
-				}
 
-				if(fn_flag==FnDown)	//Fn键按下
+			if(fn_flag==FnDown)	//Fn键按下
+			{
+				if(tmp==0x2B)//Fn+F//重新连接
 				{
-					if(tmp==0x2B)//Fn+F//重新连接
+					tx_buf[6]=0x00;
+					reconn_flag=1;//重新连接标志置1
+				}
+				if(tmp==0x5D)//Fn+A=1c///0x5D(ok)//关机标志
+				{
+					tx_buf[6]=0x00;
+					poweroff_flag=1;//关机标志置1
+				}
+				if(tmp==0x64)//Fn+S=1b///0x4A/67(BAT)//电池电量
+				{
+					ADC0_Init();
+					tx_buf[6]=batt[bat_val/100];
+					tx_buf[7]=batt[bat_val%100/10];
+					tx_buf[8]=batt[bat_val%10];
+					tx_buf[9]=batt[10];//B
+					tx_buf[10]=batt[11];//A
+					tx_buf[11]=batt[12];//T
+
+				}
+				if(tmp==0x1B)//Fn+S=1b//延长自动关机时间至
+				{
+					if(idle_timeout_time<600)
 					{
-						tx_buf[6]=0x00;
-						reconn_flag=1;//重新连接标志置1
-					}
-					if(tmp==0x5D)//Fn+A=1c///0x5D(ok)//关机标志
-					{
-						tx_buf[6]=0x00;
-						poweroff_flag=1;//关机标志置1
-					}
-					if(tmp==0x64)//Fn+S=1b///0x4A/67(BAT)//电池电量
-					{
-						ADC0_Init();
-						tx_buf[6]=batt[bat_val/100];
-						tx_buf[7]=batt[bat_val%100/10];
-						tx_buf[8]=batt[bat_val%10];
-						tx_buf[9]=batt[10];//B
-						tx_buf[10]=batt[11];//A
-						tx_buf[11]=batt[12];//T
-						
-					}
-					if(tmp==0x1B)//Fn+S=1b//延长自动关机时间至
-					{
-						if(idle_timeout_time<600)
-						{
-							idle_timeout_time=1800;//30min
-							
-							tx_buf[6]=batt[3];
-							tx_buf[7]=batt[0];
-						}
-						else
-						{
-							idle_timeout_time=300;//5min
-							tx_buf[6]=batt[0];
-							tx_buf[7]=batt[5];
-						}
-						
-						tx_buf[8]=batt[13];//m
-						tx_buf[9]=batt[14];//i
-						tx_buf[10]=batt[15];//n
-						tx_buf[11]=0x00;
+						idle_timeout_time=1800;//30min
+
+						tx_buf[6]=batt[3];
+						tx_buf[7]=batt[0];
 					}
 					else
 					{
-						tx_buf[6]=fn_key_code[tmp];
+						idle_timeout_time=300;//5min
+						tx_buf[6]=batt[0];
+						tx_buf[7]=batt[5];
 					}
+
+					tx_buf[8]=batt[13];//m
+					tx_buf[9]=batt[14];//i
+					tx_buf[10]=batt[15];//n
+					tx_buf[11]=0x00;
+				}
+				else
+				{
+					tx_buf[6]=fn_key_code[tmp];
 				}
 			}
-	
-			break;
 		}
-	} 
+
+		break;
+	}
+	}
 
 }
 
@@ -609,22 +625,22 @@ void KEY_VAL(uint8 tmp)
 	delay_ms(1);  //n=600,1ms,11.0592MHz
 	delay_us(1);  //1us
 
-*/	
-/******************************delay time********************************** 
-void delay_us(uint8 us)  
-{  
- while(us--); 
-} 																		*/ 
+*/
+/******************************delay time**********************************
+void delay_us(uint8 us)
+{
+ while(us--);
+} 																		*/
 //**************************
-void delay_ms(uint16 ms)  
-{  
- uint16 n ;  
- while(ms--)  
-      {  
-       n  =  500;   
-       while(n--);  
-      }  
-} 
+void delay_ms(uint16 ms)
+{
+	uint16 n ;
+	while(ms--)
+	{
+		n  =  500;
+		while(n--);
+	}
+}
 //-----------------------------------------------------------------------------
 // End Of File
 //-----------------------------------------------------------------------------
