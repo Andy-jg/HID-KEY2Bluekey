f86cef0f9edba9b1f0c0370b43a240074bcd8907
jiajia <674663334@qq.com>
2018-10-09 20:42:59 +0800

    分开。。把定义重新整理。

--- UART接收HID发送soft/F330_UART.c
+++ UART接收HID发送soft/F330_UART.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include "mytype.h"
 #include "config.h"
+#include"F330_UART.h"
 
 static uint16 idle_timeout_time=300;//无按键按下超时,5min
 static uint16 idle_timeout=0;//无按键按下超时
@@ -16,29 +17,7 @@ static uint8 adc_flag=0;//电池电压测试标志，10min
 static uint16 adc_timeout=0;//电压测试时间
 uint8 code batt[16]={0x27,0x1E,0x1F,0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x05,0x04,0x17,0x10,0x0C,0x11};//0~9,bat,min
 
-#define vcc_on	1
-#define vcc_off	0
-#define bl_on	0
-#define bl_off	1
-//-----------------------------------------------------------------------------
-// Global CONSTANTS
-//-----------------------------------------------------------------------------
 
-#define SYSTEMCLOCK      6125000           // SYSCLK frequency in Hz
-#define BAUDRATE         57600           // Baud rate of UART in bps
-
-#define TIMER_PRESCALER            48  // Based on Timer CKCON settings
-
-// There are SYSTEMCLOCK/TIMER_PRESCALER timer ticks per second, so
-// SYSCLK/TIMER_PRESCALER/1000 timer ticks per millisecond.
-#define TIMER_TICKS_PER_MS  SYSTEMCLOCK/TIMER_PRESCALER/1000
-
-// Note: TIMER_TICKS_PER_MS should not exceed 255 (0xFF) for the 8-bit timer
-
-#define AUX1     TIMER_TICKS_PER_MS
-#define AUX2     -AUX1
-
-#define TIMER0_RELOAD_HIGH       AUX2  // Reload value for Timer0 high byte
 
 //-----------------------------------------------------------------------------
 // Function PROTOTYPES
